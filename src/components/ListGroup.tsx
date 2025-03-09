2cb5ddccfc89bfdbe9c3fe30ed7e7e77fc747c21
nethmee03 <nethmeemudannayake@gmail.com>
2025-03-09 17:16:55 +0530

    Updated ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,12 +3,16 @@ import { Fragment } from "react";
 function ListGroup() {
     let items = ['New York','Los Angeles','Chicago','London','Paris',];
     items = [];  
+    const message = items.length === 0 ? <p>There are no items in the list</p> : null;
      
+    const getMessage = () => {
+        return items.length === 0 ? <p>There are no items in the list</p> : null;
 
+    }
   return (
     <Fragment>
         <h1>List</h1>
-        {items.length === 0 ? <p>There are no items in the list</p> : null}
+        {getMessage(1)}
         <ul className="list-group">
         {items.map ((item => 
         <li key = {item}>{item}</li>))}
