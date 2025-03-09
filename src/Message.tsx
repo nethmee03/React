18ce8cb4e59ea4b54b4ddcff6d8269cd03efa19f
nethmee03 <nethmeemudannayake@gmail.com>
2025-03-09 17:02:43 +0530

    Create ListGroup

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+    const name = 'Nethmee';
+    if (name)
+        return <h1>Hello {name}</h1>; // this is Javascript XML
+    return <h1>Hello World</h1>
+}
+
+export default Message;
